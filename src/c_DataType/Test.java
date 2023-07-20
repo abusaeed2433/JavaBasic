58d9d31a62c59081af0394188342e5dc37afc4ce
Abu Saeed <abusaeed2433@gmail.com>
2023-07-20 16:17:50 +0600

    Data type updated

--- src/c_DataType/Test.java
+++ src/c_DataType/Test.java
@@ -1,4 +1,34 @@
 package c_DataType;
 
 public class Test {
+
+    public static void main(String[] args) {
+        int n1 = 100;
+        int n2 = 102;
+
+        int res = add(n1,n2);
+        System.out.println(res); // 202
+        System.out.println(n1); // 100
+
+
+        Human human = new Human("Saeed",21);
+        printHumanData(human); // Saeed -> 21
+
+        System.out.println(human.getAge()); // 24
+
+    }
+
+    private static void printHumanData(Human human){
+        System.out.println(human.getName()+" -> "+human.getAge());
+
+        human.setAge(24); // will modify main object
+    }
+
+    private static int add(int num1, int num2){
+        int res = num1+num2;
+
+        num1 = 25; //won't affect real data
+        return res;
+    }
+
 }
